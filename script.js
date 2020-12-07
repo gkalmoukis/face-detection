57c33f7107c1511e20284b24aa6bd3ca5be788c4
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2020-12-07 19:55:50 +0200

    Add console log detections

--- script.js
+++ script.js
@@ -38,10 +38,14 @@ video.addEventListener('play', () => {
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    console.info(detections);
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  
   }, 100)
 })
